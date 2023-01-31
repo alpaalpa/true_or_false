f94eb24b7d4c6fc2b26b6899f86fec9d78854498
Albert Pang <albert.pang@hpe.com>
2023-01-31 19:44:27 +0800

    add environ_true_or_false

--- true_or_false/true_or_false.py
+++ true_or_false/true_or_false.py
@@ -1,9 +1,10 @@
 from logging import getLogger
+import os
 
 logger = getLogger(__name__)
 
 
-def true_or_false(s):
+def true_or_false(s, none_is_false=True):
     ''' Determine (educated guess) whether an input value is True
     or False.  Input can be a bool, dict, int or str.  This is
     useful for determining the value of a parameter as passed
@@ -21,12 +22,23 @@ def true_or_false(s):
         dict: {}  # empty dictionary
         list: []  # empty list
 
+    Parameter
+    ---------
+    none_is_false: bool
+        if none_is_false is True, treat None as False
+        if none_is_false is False, return None if `s` is None
+
     '''
-    TRUES = ['true', 't', '1', 'yes', 'y', 't', 'oui']
-    FALSES = ['false', 'f', '0', 'no', 'n', 'non']
+    TRUES = ['true', 't', '1', 'yes', 'y', 't', 'oui' 'ja', 'si' 'da', 'ano',
+             'jah', 'haan']
+    FALSES = ['false', 'f', '0', 'no', 'n', 'non', 'nein', 'net', 'ne',
+              'nee', 'nej', 'nahin']
 
     if s is None:
-        return False
+        if none_is_false:
+            return False
+        else:
+            return None
     if isinstance(s, bool):
         return s
     if isinstance(s, int):
@@ -45,3 +57,23 @@ def true_or_false(s):
     else:
         logger.warning(f"Cannot determine True/False from {s}")
         return None
+
+
+def environ_true_or_false(env_var: str, default=None) -> bool:
+    ''' Check whether `env_var` contains a 'true' like value such as
+           1, true, y, yes, t, oui
+        Use default value if the environment variable does not exist.
+
+        Parameters
+        ----------
+        env_var: str
+            A valid environment variable string
+        default:
+            The default value if `env_var` is not set
+
+        Returns
+        -------
+        bool or None
+
+    '''
+    return true_or_false(os.environ.get(env_var, default), none_is_false=False)
